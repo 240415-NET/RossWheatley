8e4ce7a4556894f631c9abcd25e64c777960b31b
ross-g-revpro <rossw3653@gmail.com>
2024-05-08 22:13:38 -0400

    asdfadsf

--- Week1/Hackathon/obj/Debug/net8.0/Hackathon.AssemblyInfo.cs
+++ Week1/Hackathon/obj/Debug/net8.0/Hackathon.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Hackathon")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b5e9d3f8f5865436858f01b3bd5621f2cd60fa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d44b0769b3716f3df308de5caec208e9508ca77d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Hackathon")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Hackathon")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
